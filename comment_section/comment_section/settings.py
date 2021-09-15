1c0c60c6d27f862e545ae8e047da7ff83bdb27a1
Ali Daley <ali.daley72@gmail.com>
2021-09-15 14:17:01 -0400

    Created the Models and Serializers

--- comment_section/comment_section/settings.py
+++ comment_section/comment_section/settings.py
@@ -11,16 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dfl6bayh7b(0po6$&^udu5^q5oomrtd^a^yehtnxez!c9^$k!o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'comments.apps.Comments.Config'
+    'comments'
 ]
 
 MIDDLEWARE = [
