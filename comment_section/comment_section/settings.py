e54ec8b417c6daece23138946e7889093d4cc092
Vernon Roberts <nakkiasr@live.com>
2021-09-15 11:55:26 -0700

    Pulling updates.

--- comment_section/comment_section/settings.py
+++ comment_section/comment_section/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dfl6bayh7b(0po6$&^udu5^q5oomrtd^a^yehtnxez!c9^$k!o'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'comment_section.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
