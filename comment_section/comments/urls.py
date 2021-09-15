e9fe908974acc48efedf506d6205f899ad83528c
Vernon Roberts <nakkiasr@live.com>
2021-09-15 14:50:43 -0700

    Added Reply function. Testing in Postman.

--- comment_section/comments/urls.py
+++ comment_section/comments/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comments/', views.Commentlist.as_view())
+    path('comments/', views.Commentlist.as_view()),
+    path('comments/<int:pk>/', views.Commentdetail.as_view()),
+    path('reply/', views.Replylist.as_view()),
+    path('reply/<int:pk>/', views.Replydetail.as_view())   
 ]
--- comment_section/comments/views.py
+++ comment_section/comments/views.py
@@ -4,6 +4,7 @@ from .serializers import CommentSerializer, ReplySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 # Create your views here.
 
 class Commentlist(APIView):
@@ -14,6 +15,54 @@ class Commentlist(APIView):
     
     def post(self,request):
         serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Commentdetail(APIView):
+
+    def get_comment(self, pk):
+        try:
+           return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_comment(pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+class Replylist(APIView): 
+    def get(self,request):
+        reply = Reply.objects.all()
+        serializer = ReplySerializer(reply, many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = ReplySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Replydetail(APIView):
+    
+    def get_id(self, pk):
+        try:
+           return Reply.objects.get(pk=pk)
+        except Reply.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        comment = self.get_id(pk)
+        serializer = ReplySerializer(comment)
+        return Response(serializer.data)
+
+
+    def post(self,request, pk):
+        reply = self.get_id(pk)
+        serializer = ReplySerializer(reply, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
